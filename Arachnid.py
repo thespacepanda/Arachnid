3ccdc503cc9ef4141e8bc1458fd3db951c730096
Britt Mathis <britt.mathis@gmail.com>
2015-04-02 23:56:58 -0400

    Now continuously polls

--- Arachnid.py
+++ Arachnid.py
@@ -1,6 +1,7 @@
 import DownloadManager
 import LinkManager
 import TargetFilter
+import time
 
 class Arachnid(object):
     """Arachnid manages the life cycle of the other objects"""
@@ -9,9 +10,19 @@ class Arachnid(object):
         self._link_manager = LinkManager.LinkManager()
 
     def run_downloader(self):
-       url_set = self._link_manager.get_links()
-       pdf_set = TargetFilter.target_filter(url_set)
-       DownloadManager.download_urls(pdf_set)
+        print("running downloader...")
+        while True:
+            print("getting links...")
+            url_set = self._link_manager.get_links()
+            print("got links")
+            print("filtering for pdfs...")
+            pdf_set = TargetFilter.target_filter(url_set)
+            print("just pdfs now")
+            print("downloading pdfs...")
+            DownloadManager.download_urls(pdf_set)
+            print("sleeping 30 seconds")
+            time.sleep(30)
+            print("done sleeping!")
 
 if __name__ == "__main__":
     arachnid = Arachnid()
