ed8e8791e4fe6cb0b6d61ac4222453a26035d5f0
Britt Mathis <britt.mathis@gmail.com>
2015-04-09 21:34:30 -0400

    Simplified Arachnid

--- Arachnid.py
+++ Arachnid.py
@@ -1,26 +1,15 @@
 import DownloadManager
 import LinkManager
 import TargetFilter
-import time
 
-class Arachnid(object):
-    """Arachnid manages the life cycle of the other objects"""
-
-    def __init__(self):
-        self._link_manager = LinkManager.LinkManager()
-
-    def run_downloader(self):
-        print("running downloader...")
-        while True:
-            for url in self._link_manager.get_links():
-                try:
-                    print("Current URL: {}".format(url))
-                    if TargetFilter.target_filter(url):
-                        print("{} matches target filter".format(url))
-                        DownloadManager.download_url(url)
-                except:
-                    print("Failed on the current url for some reason")
+def main():
+    while True:
+        for url in LinkManager.LinkManager().get_links():
+            try:
+                if TargetFilter.target_filter(url):
+                    DownloadManager.download_url(url)
+            except:
+                print("failed on current url")
 
 if __name__ == "__main__":
-    arachnid = Arachnid()
-    arachnid.run_downloader()
+    main()
